d9826a0809e8c20183973f5ea4dda834a7cb37bd
vipulpathak113 <vipulpathak113@gmail.com>
2022-05-24 21:36:30 +0530

    redux implementation

---
+++ src/Redux/reducer/homeReducer.js
@@ -0,0 +1,3 @@
+export default function homeReducer (state, action){
+  return null;
+};
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
+import RootReducer from './Redux/reducer'
+
+const middleware = applyMiddleware(thunk);
+const store = createStore(RootReducer, middleware);
+
+export default store;
+
+
